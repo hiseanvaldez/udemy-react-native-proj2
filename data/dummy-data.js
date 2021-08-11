70897faf0b17a6e4af80e57545fcc74d36a4bfb5
Shan Valdez <hisean.valdez@gmail.com>
2021-08-11 20:56:10 +0800

    Fix category id.

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -5,7 +5,7 @@ export const CATEGORIES = [
   new Category("c1", "Italian", "#f5428d"),
   new Category("c2", "Quick & Easy", "#f54242"),
   new Category("c3", "Hamburgers", "#f5a442"),
-  new Category("c04", "Filipino", "F19292"),
+  new Category("c4", "Filipino", "F19292"),
   new Category("c5", "Light & Lovely", "#368dff"),
   new Category("c6", "Exotic", "#41d95d"),
   new Category("c7", "Breakfast", "#9eecff"),
